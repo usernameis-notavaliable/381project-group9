18ea46311af2e3296bf145cb26f434fbdf206159
usernameis-notavaliable <150214233+usernameis-notavaliable@users.noreply.github.com>
2024-11-28 23:49:51 +0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,10 +6,6 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  username: {
-    type: String,
-    required: true
-  },
   displayName: {
     type: String,
     required: true
